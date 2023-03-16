6b329334af86bccb0ca35b44657734061202ff06
mariana-reis <mariana.reis@al.resilia.com.br>
2023-03-16 18:14:29 -0300

    :sparkles: feat: templates

---
+++ templates/footer.php
@@ -0,0 +1,29 @@
+<footer id="footer">
+    <div id="social-container">
+      <ul>
+        <li>
+          <a href="#"><i class="fab fa-facebook-square"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </li>
+      </ul>
+    </div>
+    <div id="footer-links-container">
+      <ul>
+        <li><a href="#">Adicionar filme</a></li>
+        <li><a href="#">Adicionar critica</a></li>
+        <li><a href="#">Entrar / Registrar</a></li>
+      </ul>
+    </div>
+    <p>&copy; 2023 Mariana Silva</p>
+  </footer>
+  <!-- bootstrap js  -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/js/bootstrap.js" integrity="sha512-L6XANV6sOsx9N9c787eDN1pjB2Pzautd3xDgn4cMKuoleHSuCJi5pCDGPCtwE3Bd4A1Olnr0k0aQXbczYzg+wg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <!-- end bootstrap js  -->
+</body>
+</html>
