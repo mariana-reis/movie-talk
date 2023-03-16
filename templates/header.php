bfa35638274fd4f204fa43c323a146ecf3496e6b
mariana-reis <mariana.reis@al.resilia.com.br>
2023-03-16 20:36:10 -0300

    :lipstick: feat: add favicon

--- templates/header.php
+++ templates/header.php
@@ -25,6 +25,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="<?= $BASE_URL ?>img/favicon.ico" type="image/x-icon">
   <title>MovieStar</title>
   <link rel="stylesheet" href="<?= $BASE_URL ?>img/moviestar.png" />
   <!-- bootstrap  -->
